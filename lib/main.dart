f627f25680bce3fb2c77561119fb283c3af9d26f
Royleong31 <65440011+Royleong31@users.noreply.github.com>
2021-03-05 17:59:45 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> rowWidgets = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+
+  int questionIndex = 0;
+  int score = 0;
+  var questionObj = [];
+
+  void checkAns(bool response) {
+    if (response == questionObj[questionIndex].answer) {
+      score++;
+      rowWidgets.add(Icon(Icons.check, color: Colors.green));
+    } else {
+      rowWidgets.add(Icon(
+        Icons.close,
+        color: Colors.red,
+      ));
+    }
+    print(score);
+    if (questionIndex < questions.length - 1)
+      questionIndex++;
+    else
+      return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(questions.length);
+
+    for (int i = 0; i < questions.length; i++) {
+      var newQ = Question(questions[i], answers[i]);
+      questionObj.add(newQ);
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questionObj[questionIndex].question,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() => checkAns(true));
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() => checkAns(false));
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Row(children: rowWidgets),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
---
+++ lib/question.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String question;
+  final bool answer;
+
+  Question(this.question, this.answer);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      
+    );
+  }
+}
